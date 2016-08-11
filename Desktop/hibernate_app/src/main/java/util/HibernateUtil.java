1e076f7b289955d37895c08d356af7ebd32aed70
sama <s.farsi70@yahoo.com>
2016-08-11 14:03:55 +0430

    ss

--- Desktop/hibernate_app/src/main/java/util/HibernateUtil.java
+++ Desktop/hibernate_app/src/main/java/util/HibernateUtil.java
@@ -1,5 +1,23 @@
 package util;
 
 
+import org.hibernate.SessionFactory;
+import org.hibernate.cfg.Configuration;
+
 public class HibernateUtil {
+    private static SessionFactory sessionFactory = null;
+    private HibernateUtil() {
+    }
+    static {
+        try {
+            sessionFactory = new Configuration().configure().buildSessionFactory();
+        } catch (Exception e) {
+            System.err.println("Initial SessionFactory creation failed." + e);
+            throw new ExceptionInInitializerError(e);
+        }
+    }
+
+    public static SessionFactory getSessionFactory() {
+        return sessionFactory;
+    }
 }
